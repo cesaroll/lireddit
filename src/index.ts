1a74b79dc94a5b5b59f0a4db605dfe755b1d159a
Cesar Lopez <cesarlerdeut@gmail.com>
2022-07-10 11:16:26 +0200

    Connecting to DB

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import { MikroORM } from "@mikro-orm/core";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
-  const orm = await MikroORM.init({
+  console.log('Running main');
+
+  const orm = await MikroORM.init<PostgreSqlDriver>({
+    entities: ['./dist/entities'],
+    entitiesTs: ['./src/entities'],
     dbName: 'lireddit',
     type: 'postgresql',
-    debug: process.env.NODE_ENV !== 'production'
+    debug: !__prod__,
   });
+
+  console.log('Creating Posts');
+  const post = orm.em.create(Post, {title: 'my first post'} as Post);
+  await orm.em.persistAndFlush(post);
+  console.log('--------------------sql2----------------');
+  await orm.em.nativeInsert(Post, {title: 'my first post'});
+
 }
 
-main();
+main().catch(e => {
+  console.error(e);
+});
