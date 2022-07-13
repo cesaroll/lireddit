94631a7b41e34e413b5b02e8c24e741e5e1a2688
Cesar Lopez <cesarlerdeut@gmail.com>
2022-07-14 00:48:26 +0200

    Setup Front End and add register form

---
+++ lireddit-web/src/components/InputField.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+import { FieldHookConfig, useField } from 'formik';
+
+type InputFieldProps = FieldHookConfig<any> & {
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      >
+      </Input>
+      { error && <FormErrorMessage>{error}</FormErrorMessage> }
+    </FormControl>
+  );
+};
+
+export default InputField;
