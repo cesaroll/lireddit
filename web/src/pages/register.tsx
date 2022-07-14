0f8fd68f6ab2f8c2cb94eef0c89cadb92fad8883
Cesar Lopez Lerma <clopezlerma@wayfair.com>
2022-07-14 15:10:23 +0200

    Map server error to Formik format and set Errors

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,6 +4,7 @@ import { Box, Button } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
 
 interface RegisterProps {}
 
@@ -14,8 +15,11 @@ const [, register] = useRegisterMutation();
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={ async (values) => {
+        onSubmit={ async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          }
         }}
       >
         {({ isSubmitting }) => (
